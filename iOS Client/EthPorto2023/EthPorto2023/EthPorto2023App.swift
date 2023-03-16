52b758c06fd81fcc5f2835d239fe7e8f7608c1fa
Alok Sahay <alok.sahay87@gmail.com>
2023-03-16 18:06:53 +0000

    App setup

---
+++ iOS Client/EthPorto2023/EthPorto2023/EthPorto2023App.swift
@@ -0,0 +1,17 @@
+//
+//  EthPorto2023App.swift
+//  EthPorto2023
+//
+//  Created by Alok Sahay on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct EthPorto2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
